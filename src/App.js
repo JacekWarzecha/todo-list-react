4dab3426fbd5f55f6f6bd802b85bdfeb517745e6
JacekWarzecha <drjackys@gmail.com>
2023-01-24 18:28:50 +0100

    hideDone interaction added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
+import { useState } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import RemoteButtons from "./RemoteButtons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import React from "react";
 
 const tasks = [
   { id: 1, content: "Przejść na reacta", done: false },
   { id: 2, content: "Zjeść obiad", done: true },
 ];
 
-const hideDone = false;
-
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone((hideDone) => !hideDone);
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -22,7 +28,13 @@ function App() {
       <Section
         title="Lista zadań"
         body={<Tasks tasks={tasks} hideDone={hideDone} />}
-        extraHeaderContent={<RemoteButtons tasks={tasks} hideDone={hideDone} />}
+        extraHeaderContent={
+          <RemoteButtons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+          />
+        }
       />
     </Container>
   );
